8a609206632a2288c7b5e7d8fd2da870d266e11a
SooryaSDas <sooryasandra6@gmail.com>
2023-12-21 15:07:43 +0530

    first commit

---
+++ src/Components/Buy.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+const Buy = () => {
+  return (
+    <div>
+            <div className="w-[1170px] h-[222px] py-6 bg-white border-b border-slate-300 flex-col justify-center items-start gap-6 inline-flex">
+    <div className="self-stretch justify-start items-center gap-6 inline-flex">
+        <div className="grow shrink basis-0 flex-col justify-center items-start gap-3 inline-flex">
+            <div className="text-slate-900 text-[26px] font-normal font-['Dessau'] leading-[38px]">Kent, Washington 98031</div>
+            <div className="flex-col justify-center items-start gap-4 flex">
+                <div className="flex-col justify-center items-start gap-3 flex">
+                    <div className="justify-start items-center gap-2.5 inline-flex">
+                        <div className="text-slate-500 text-xl font-normal font-['Dessau'] leading-normal">106 SE 22905 Place SE</div>
+                        <div className="w-px self-stretch bg-zinc-300" />
+                        <div className="justify-center items-center gap-1.5 flex">
+                            <div className="w-2 h-2 bg-red-500 rounded-full" />
+                            <div className="text-red-500 text-xl font-normal font-['Dessau'] leading-normal">For Sale</div>
+                        </div>
+                        <div className="w-px self-stretch bg-zinc-300" />
+                        <div className="justify-center items-center gap-1 flex">
+                            <div className="w-4 h-4 relative" />
+                            <div className="text-slate-500 text-xl font-normal font-['Dessau'] leading-normal">10 months ago</div>
+                        </div>
+                    </div>
+                    <div className="justify-start items-center gap-2.5 inline-flex">
+                        <div className="text-slate-500 text-xl font-normal font-['Dessau'] leading-normal">3 Beds</div>
+                        <div className="w-px self-stretch bg-zinc-300" />
+                        <div className="justify-center items-center gap-1.5 flex">
+                            <div className="text-slate-500 text-xl font-normal font-['Dessau'] leading-normal">2 Baths</div>
+                        </div>
+                        <div className="w-px self-stretch bg-zinc-300" />
+                        <div className="text-slate-500 text-xl font-normal font-['Dessau'] leading-normal">1,440 SqFt</div>
+                        <div className="w-px self-stretch bg-zinc-300" />
+                        <div className="text-slate-500 text-xl font-normal font-['Dessau'] leading-normal">0.190 Acres</div>
+                    </div>
+                </div>
+                <div className="justify-start items-center gap-4 inline-flex">
+                    <div className="px-4 py-3 bg-stone-50 rounded-3xl justify-start items-center gap-2.5 flex">
+                        <div className="w-6 h-6 relative" />
+                        <div className="text-slate-900 text-[22px] font-normal font-['Dessau'] leading-normal">Save</div>
+                    </div>
+                    <div className="px-4 py-3 bg-stone-50 rounded-3xl justify-center items-center gap-1.5 flex">
+                        <div className="justify-center items-center gap-2.5 flex">
+                            <div className="w-6 h-6 relative">
+                                <div className="w-[12.41px] h-[9.18px] left-[16.14px] top-[4px] absolute origin-top-left rotate-90 justify-center items-center inline-flex">
+                                    <div className="w-[14.38px] h-[10.66px] relative origin-top-left rotate-180 flex-col justify-start items-start flex">
+                                        <div className="w-[13px] h-[0px] origin-top-left rotate-180 border-2 border-slate-900"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="text-slate-900 text-[22px] font-normal font-['Dessau'] leading-normal">Share</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="grow shrink basis-0 self-stretch flex-col justify-end items-end gap-4 inline-flex">
+            <div className="flex-col justify-center items-start gap-4 flex">
+                <div className="text-slate-900 text-[40px] font-normal font-['Dessau'] leading-[46px]">$4,667</div>
+            </div>
+            <div className="self-stretch justify-end items-center gap-4 inline-flex">
+                <div className="h-14 px-6 py-5 rounded-[30px] border border-blue-500 flex-col justify-center items-center inline-flex">
+                    <div className="pb-0.5 justify-start items-center gap-2.5 inline-flex">
+                        <div className="text-blue-500 text-lg font-normal font-['Dessau'] leading-tight">Download Printable Flyer</div>
+                    </div>
+                </div>
+                <div className="h-14 px-6 py-5 bg-blue-500 rounded-[30px] flex-col justify-center items-center inline-flex">
+                    <div className="pb-0.5 justify-start items-center gap-2.5 inline-flex">
+                        <div className="text-neutral-50 text-lg font-normal font-['Dessau'] leading-tight">Submit a tour request</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Buy
